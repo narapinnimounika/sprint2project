91a77a2fb03c40a71024a919d76f84b9107de311
Mounika <mounikanarapinni2000@gmail.com>
2023-07-27 09:22:38 +0530

    sprint2

---
+++ js/ViewProduct.js
@@ -0,0 +1,45 @@
+// Initialize a new TaskManager with currentId set to 0
+const itemsController = new ItemsController(0);
+
+
+function addItemCard(item){
+    const itemHTML = '<div class="card" style="width: 20rem;">\n' +
+        '    <img src="'+item.img +'" width="300" height="250"  alt="product image">\n' +
+        '    <div class="card-body">\n' +
+        '        <h5 class="card-title">'+item.name+'</h5>\n' +
+        '        <p class="card-text">'+item.description+'</p>\n' +
+        '        <a href="#" class="btn btn-primary">Add</a>\n' +
+        '    </div>\n' +
+        '</div>\n' +
+        '<br/>';
+    const itemsContainer = document.getElementById("list-items");
+    itemsContainer.innerHTML += itemHTML;
+    //i++; // i = i+1;
+    //i+=2; // i = i + 2;
+    //str += s; // str = str + s;
+    //innerHTML = innerHTML + itemHTML;
+}
+
+function loadStorageSampleData(){
+    if(!localStorage.getItem("items")){
+        const sampleItems = [{'name':'juice',
+        'img':'https://www.gs1india.org/media/Juice_pack.jpg',
+        'description':'Orange and Apple juice fresh and delicious'},
+        {'name':'Ruffles Chicken',
+        'img':'https://s3-ap-southeast-1.amazonaws.com/www8.fairprice.com.sg/fpol/media/images/product/XL/13086598_LXL1.jpg',
+        'description':'Ruffles Potato Chips - Chicken'}];
+        localStorage.setItem("items", JSON.stringify(sampleItems));
+    }
+}
+
+function loadCardsListFromItemsController(){
+    console.log('start');
+    for(var i = 0, size = itemsController.items.length; i < size ; i++){
+        const item = itemsController.items[i];
+        addItemCard(item);
+    }
+}
+
+loadStorageSampleData();
+itemsController.loadItemsFromLocalStorage();
+loadCardsListFromItemsController();
